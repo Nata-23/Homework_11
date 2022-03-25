176f9aba4f9ce61abc3fb3d9d218311bf1401af3
Nataliia <d_tasha@ukr.net>
2022-03-25 17:13:24 +0100

    .than/.catch

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 "use strict";
+//1
+let promise = new Promise(function (resolve, reject) {
+  setTimeout(() => resolve("done"), 5000);
+});
+promise
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error))
+  .finally(() => console.log("Promise was finished!"));
+//2
