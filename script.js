84d6cc4db78dee05c808320f0787e18712f0cd71
Nataliia <d_tasha@ukr.net>
2022-03-25 20:38:10 +0100

    try/catch

--- script.js
+++ script.js
@@ -3,8 +3,21 @@
 let promise = new Promise(function (resolve, reject) {
   setTimeout(() => resolve("done"), 5000);
 });
-promise
-  .then((result) => console.log(result))
-  .catch((error) => console.error(error))
-  .finally(() => console.log("Promise was finished!"));
-//2
+// promise
+//   .then((result) => console.log(result))
+//   .catch((error) => console.error(error))
+//   .finally(() => console.log("Promise was finished!"));
+
+//2 async / await
+async function getPromise() {
+  try {
+    const result = await promise;
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    console.log("Promise was finished!");
+  }
+}
+
+getPromise();
